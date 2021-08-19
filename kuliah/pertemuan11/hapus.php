d9dccebb9bdab16ba972139cc8796a52c67cf940
RifkiFiransah <muhammadrifkifiransah@gmail.com>
2021-08-19 14:13:08 +0700

    Menambahkan Fitur tambah, hapus dan pencarian data

---
+++ kuliah/pertemuan11/hapus.php
@@ -0,0 +1,20 @@
+<?php
+require 'functions.php';
+
+// Jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header('Location: index.php');
+  exit;
+}
+
+// Ambil id dari url
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+  echo "<script>
+          alert('Data Berhasil Di Hapus');
+          document.location.href = 'index.php';
+        </script>";
+} else {
+  echo "Data Gagal Dihapus";
+}
